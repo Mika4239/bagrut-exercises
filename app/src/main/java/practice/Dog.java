cc83a870445150196b0034ee5bd5cbad0ed9170a
Mika <mikush.carmel@gmail.com>
2022-05-27 15:47:22 +0300

    added parctice exercises

---
+++ app/src/main/java/practice/Dog.java
@@ -0,0 +1,38 @@
+package practice;
+
+public class Dog {
+
+  private String name;
+  private String type;
+  private int age;
+
+  public Dog(String name, String type, int age) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+  }
+
+  public void setName(String type) {
+    this.type = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+}
---
+++ app/src/main/java/practice/Main.java
@@ -0,0 +1,16 @@
+package practice;
+
+import bagtut_ex.Node;
+import bagtut_ex.Stack;
+import java.util.Scanner;
+
+public class Main {
+
+  public static Scanner reader = new Scanner(System.in);
+
+  public static void main(String[] args) {}
+
+  public static Node<Dog> deletePudels(Stack<Dog> dogs) {
+    return new Node<Dog>(new Dog("dog", "pudel", 5));
+  }
+}
