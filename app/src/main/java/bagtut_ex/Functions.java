2f9c2d4cb141b99bf58c7cc07d45c6140442189a
Mika <mikush.carmel@gmail.com>
2022-05-27 15:48:40 +0300

    added functions

--- app/src/main/java/bagtut_ex/Functions.java
+++ app/src/main/java/bagtut_ex/Functions.java
@@ -6,7 +6,8 @@ public class Functions {
 
   public static Scanner reader = new Scanner(System.in);
 
-  public static BinNode<Integer> createNode() {
+  // tree functions
+  private static BinNode<Integer> createNode() {
     System.out.println("enter value (-1) to end: ");
     return createTree();
   }
@@ -24,15 +25,39 @@ public class Functions {
     return new BinNode<Integer>(value, left, right);
   }
 
+  private static BinNode<String> createStringNode() {
+    System.out.println("enter value (-1) to end: ");
+    return createStringTree();
+  }
+
+  public static BinNode<String> createStringTree() {
+    String value;
+    value = reader.next();
+    if (value.equals("-1")) {
+      return null;
+    }
+    System.out.println("enter left of " + value + " (-1 to end)");
+    BinNode<String> left = createStringNode();
+    System.out.println("enter right of " + value + " (-1 to end)");
+    BinNode<String> right = createStringNode();
+    return new BinNode<String>(value, left, right);
+  }
+
+  // linked list functions
   public static Node<Integer> createList() {
     int num;
+    Node<Integer> last;
     System.out.println("Enter a number (-999 to end): ");
     num = reader.nextInt();
     Node<Integer> list = new Node<Integer>(num);
+    last = list;
+    System.out.println("Enter a number (-999 to end): ");
+    num = reader.nextInt();
     while (num != -999) {
+      last.setNext(new Node<Integer>(num));
+      last = last.getNext();
       System.out.println("Enter a number (-999 to end): ");
       num = reader.nextInt();
-      addToEnd(list, num);
     }
 
     return list;
@@ -47,6 +72,59 @@ public class Functions {
     tmp.setNext(head);
   }
 
+  public static Node<Character> createCharList() {
+    char chr;
+    System.out.println("Enter a character (- to end): ");
+    chr = reader.next().charAt(0);
+    Node<Character> list = new Node<Character>(chr);
+    while (chr != '-') {
+      System.out.println("Enter a character (- to end): ");
+      chr = reader.next().charAt(0);
+      addCharToEnd(list, chr);
+    }
+
+    return list;
+  }
+
+  public static void addCharToEnd(Node<Character> head, char chr) {
+    Node<Character> tmp = head;
+    while (tmp.hasNext()) {
+      tmp = tmp.getNext();
+    }
+    tmp.setValue(chr);
+    tmp.setNext(head);
+  }
+
+  public static void printList(Node<Integer> list) {
+    Node<Integer> tmp = list;
+    while (tmp != null) {
+      System.out.println(tmp.getValue());
+      tmp = tmp.getNext();
+    }
+  }
+
+  public static Node<Integer> delete(Node<Integer> list, int num) {
+    if (list == null) {
+      return null;
+    }
+    if (list.getValue() == num) {
+      return list.getNext();
+    }
+    Node<Integer> prev = list;
+    Node<Integer> pos = list.getNext();
+    while (pos != null) {
+      if (pos.getValue() == num) {
+        prev.setNext(pos.getNext());
+        return list;
+      } else {
+        prev = pos;
+        pos = pos.getNext();
+      }
+    }
+    return list;
+  }
+
+  // stack functions
   public static Stack<Integer> createStack() {
     Stack<Integer> s = new Stack<>();
     System.out.println("Enter a number (-999 to end): ");
@@ -61,6 +139,32 @@ public class Functions {
     return s;
   }
 
+  public static Stack<Integer> reverse(Stack<Integer> s) {
+    Stack<Integer> tmp = new Stack<>();
+    Stack<Integer> reverseS = new Stack<>();
+    while (!s.isEmpty()) {
+      reverseS.push(s.top());
+      tmp.push(s.pop());
+    }
+    while (!tmp.isEmpty()) {
+      s.push(tmp.pop());
+    }
+
+    return reverseS;
+  }
+
+  public static void printStack(Stack<Integer> s) {
+    Stack<Integer> tmp = new Stack<>();
+    while (!s.isEmpty()) {
+      System.out.println(s.top());
+      tmp.push(s.pop());
+    }
+    while (!tmp.isEmpty()) {
+      s.push(tmp.pop());
+    }
+  }
+
+  // array functions
   public static int[] createArray() {
     System.out.println("Enter the array length: ");
     int[] arr = new int[reader.nextInt()];
@@ -71,11 +175,9 @@ public class Functions {
     return arr;
   }
 
-  public static void printList(Node<Integer> list) {
-    Node<Integer> tmp = list;
-    while (tmp != null) {
-      System.out.println(tmp.getValue());
-      tmp = tmp.getNext();
+  public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
     }
   }
 }
